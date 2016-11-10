bad2bc20b203f229afee090efcda7534ae7b08d1
shedonggui <shedonggui@meicai.cn>
2016-11-10 16:14:21 +0800

    创建工程

---
+++ weex-demo/weex-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weex-demo
+//
+//  Created by shedonggui on 16/11/10.
+//  Copyright © 2016年 shedonggui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
